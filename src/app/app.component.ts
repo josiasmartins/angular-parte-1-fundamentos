815d28172a2e4ec096df0d2492b1e659e737b096
Caitano <josiasmartinscaitano@gmail.com>
2021-10-28 22:23:09 -0300

    iniciado o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+// decorate: é uma sintaxe especial que pode colocar uma meta informação sobre uma determinada classe
+// meta-informação: crio instancia dela classe, crio um objeto
+// selector: permite usar componente em template
+// templateUrl: diz qual apresentação desse componente
+// styleUrls: fala onde o css desse componente
+
+// Date Binding: associal de dados. Faz uma asscociação com uma fonte de dados
+// expression {{title}}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = "mar";
+  url = '../assets/6457dc8f063f284fe17e519dc28b5437.jpg'
 }
