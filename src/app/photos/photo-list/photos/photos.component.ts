ab9a74648180e1a2713e33cba9b710e5216cca13
Caitano <josiasmartinscaitano@gmail.com>
2021-11-07 10:58:48 -0300

    retomado o projeto

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,14 +6,29 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   // para passar dado para esse componente, tem que ser inbount property
   @Input() photos: Photo[] = []
+  rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  // o OnChanges: recebe todas as posiveis mudanças nas inboud property no componente
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
   }
 
 }
