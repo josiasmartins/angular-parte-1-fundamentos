fd3ef938dc9f85fd6b514ec53accc0ea25917096
Caitano <josiasmartinscaitano@gmail.com>
2021-11-07 18:01:15 -0300

    organizado o código: adicionado um módulo para cada componente

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,21 @@
+import { PhotoModule } from './../photo/photo.module';
+import { CommonModule } from '@angular/common';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe.';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { PhotosComponent } from './photos/photos.component';
+import { NgModule } from '@angular/core';
+import { PhotoListComponent } from './photo-list.component';
+
+@NgModule({
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescriptionPipe
+  ],
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
+})
+export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,5 @@
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -8,16 +10,17 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe.';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
 // esse módulo vai declarar todos os componentes que diz respito a fotos
 @NgModule({
   // declarations entra tudo que o módulo tem
   declarations: [
-    PhotoComponent,
-    PhotosComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent
+    // PhotoComponent,
+    // PhotosComponent,
+    // PhotoListComponent,
+    // PhotoFormComponent,
+    // FilterByDescriptionPipe,
+    // LoadButtonComponent
   ],
   // para que ele seja visto no appModule, preciso exportar esse componente
   // exports: [ PhotoComponent],
@@ -25,8 +28,9 @@ import { LoadButtonComponent } from './photo-list/load-button/load-button.compon
     // O CommonModule tem as declaraçoes da diretivas.
     // O BrowserModule só pode ser declarado no componente principal
     // BrowserModule: além de trazer as diretivas, traz varias questões para minha aplicação angular funcione
-    CommonModule,
-    HttpClientModule,
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule
    ]
 })
 export class PhotosModule {}
