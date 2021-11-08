fcba56587b7fccc967012fcea6f7b475d10e0b3d
Caitano <josiasmartinscaitano@gmail.com>
2021-11-08 07:34:02 -0300

    fix: corrigido o erro de renderização do componente card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
+import { Component, Input, NgModule } from '@angular/core';
 
 @Component({
   selector: 'ap-card',
   templateUrl: './card.component.html'
 })
 export class CardComponent {
-  @Input() title: string = '';
+
+  @Input() title: string = ''
+
 }
+
